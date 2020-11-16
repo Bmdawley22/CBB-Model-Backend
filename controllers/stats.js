083e191520f3adbab87fba4808cf692cf02ee725
Brady Dawley <dawleybradym@johndeere.com>
2020-11-16 07:33:08 -0600

    heroku updates

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,4 +1,5 @@
 const OffStats = require('../models').off_stats_pg;
+const DefStats = require('../models').def_stats_pg;
 
 const constants = require('../constants');
 
@@ -11,7 +12,17 @@ const getAllOffStats = (req, res) => {
         res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
     })
 }
+const getAllDefStats = (req, res) => {
+    DefStats.findAll()
+    .then(defStats => {
+        res.status(constants.SUCCESS).json(defStats)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
 
 module.exports = {
-    getAllOffStats
+    getAllOffStats,
+    getAllDefStats
 }
--- routes/stats.js
+++ routes/stats.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/off-stats', ctrl.stats.getAllOffStats);
+router.get('/def-stats', ctrl.stats.getAllDefStats);
 
 
 module.exports = router;
